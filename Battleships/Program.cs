26ec31acb3b2dc41567842cf56c6395e52bb0b28
Jonathan <yonimn2000@gmail.com>
2017-12-17 10:26:23 -0500

    One player game - works

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -6,25 +6,100 @@ namespace Battleships
     class Program
     {
         public static Random random = new Random();
+        static Board myBoard = new Board();
         static void Main(string[] args)
         {
-            Board myBoard = new Board();
-            //myBoard.PrintBoard();
-            myBoard.PrintShips();
+            for (int i = 50; i > 0; i--)//Tries
+            {
+                if (myBoard.noMoreShipsFlag)
+                    break;
+                Console.WriteLine("Welcome to Jonathan's Battleships game!\n");
+                myBoard.PrintBoard();
+                Console.WriteLine("Tries remaining: " + i);
+                Console.WriteLine();
+                if (Move())//If a ship is shot
+                    i++;
+                Console.Clear();
+            }
+            if (myBoard.noMoreShipsFlag)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("You Won!\n");
+                Console.ResetColor();
+                myBoard.PrintBoard(true);
+            }
+            else
+            {
+                Console.WriteLine("Ships locations:\n");
+                myBoard.PrintBoard(true);
+                Console.WriteLine("Game Over!");
+            }
             Console.ReadLine();
+        }
+
+        private static bool Move()
+        {
+            int x = GetValidX(), y = GetValidY();
+            while (myBoard.board[y, x])
+            {
+                Console.WriteLine("\nYou have already shot to this point...\n");
+                x = GetValidX();
+                y = GetValidY();
+            }
+            return myBoard.Shoot(x, y);
+        }
+
+        private static int GetValidX()
+        {
+            int x = -1;
+            while (x < 0 || x > Board.size[1] - 1)
+            {
+                Console.WriteLine("Enter your X point:");
+                string input = Console.ReadLine();
+                try
+                {
+                    x = int.Parse(input);
+                }
+                catch { }
+                if (x < 0 || x > Board.size[1] - 1)
+                    Console.WriteLine("\nEntered point is out of range...\n");
+                }
+            return x;
+        }
+
+        private static int GetValidY()
+        {
+            int y = -1;
+            while (y < 0 || y > Board.size[0] - 1)
+            {
+                Console.WriteLine("Enter your Y point:");
+                string input = Console.ReadLine();
+                try
+                {
+                    y = int.Parse(input);
+                }
+                catch { }
+                if (y < 0 || y > Board.size[0] - 1)
+                    Console.WriteLine("\nEntered point is out of range...\n");
+            }
+            return y;
         }
     }
 
     class Board
     {
         public static int[] size = { 10, 10 };
-        int[,] shipCountAndSize = { { 1, 4 }, { 2, 3 }, { 3, 2 }, { 4, 1 } };//{ Count, Size }
+        static int[,] shipCountAndSize = { { 1, 4 }, { 2, 3 }, { 3, 2 }, { 4, 1 } };//{ Count, Size }
+        int destroyedShips = 0;
         public bool[,] board = new bool[size[0], size[1]];
         public List<Ship> ships = new List<Ship>();
+        public bool noMoreShipsFlag = false;
+
         public Board()
         {
-            for (int i = 0; i < board.GetLength(1); i++)
-                for (int j = 0; j < board.GetLength(0); j++)
+            for (int i = 0; i < board.GetLength(0); i++)
+                for (int j = 0; j < board.GetLength(1); j++)
                     board[i, j] = false;
             for (int i = 0; i < shipCountAndSize.GetLength(0); i++)
                 for (int j = 0; j < shipCountAndSize[i, 0]; j++)
@@ -34,44 +109,108 @@ namespace Battleships
         public void AddShip(int size)
         {
             ships.Add(new Ship(size));
-            PrintShips();
-            Console.WriteLine();
         }
 
-        public void PrintBoard()
+        public void PrintBoard(bool showShip = false)
         {
             Console.Write("  ");
-            for (int i = 0; i < board.GetLength(0); i++)
-                Console.Write(i + " ");
-            Console.WriteLine();
             for (int i = 0; i < board.GetLength(1); i++)
-            {
-                Console.Write(i + " ");
-                for (int j = 0; j < board.GetLength(0); j++)
-                    Console.Write((board[i, j] ? 'X' : 'O') + " ");
-                Console.WriteLine();
-            }
-        }
-
-        public void PrintShips()
-        {
-            Console.Write("  ");
-            for (int i = 0; i < Ship.isShip.GetLength(0); i++)
                 Console.Write(i + " ");
             Console.WriteLine();
-            for (int i = 0; i < Ship.isShip.GetLength(1); i++)
+            for (int i = 0; i < board.GetLength(0); i++)
             {
                 Console.Write(i + " ");
-                for (int j = 0; j < Ship.isShip.GetLength(0); j++)
-                    Console.Write((Ship.isShip[i, j] ? 'O' : '~') + " ");
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    char output = '\0';
+                    if (showShip)
+                    {
+                        if (board[i, j] && Ship.isShip[i, j])
+                            output = '*';
+                        else if (!board[i, j] && Ship.isShip[i, j])
+                            output = '+';
+                        else if (board[i, j] && !Ship.isShip[i, j])
+                            output = 'X';
+                        else
+                            output = 'O';
+                    }
+                    else
+                    {
+                        if (board[i, j] && Ship.isShip[i, j])
+                            output = '*';
+                        else if (board[i, j] && !Ship.isShip[i, j])
+                            output = 'X';
+                        else
+                            output = 'O';
+                    }
+                    switch (output)
+                    {
+                        case '*':
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            break;
+                        case '+':
+                            {
+                                Console.BackgroundColor = ConsoleColor.Magenta;
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            break;
+                        case 'X':
+                            {
+                                Console.BackgroundColor = ConsoleColor.Green;
+                                Console.ForegroundColor = ConsoleColor.Black;
+                            }
+                            break;
+                        case 'O':
+                            {
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            break;
+                    }
+                    Console.Write(output + " ");
+                    Console.ResetColor();
+                }
                 Console.WriteLine();
             }
+            Console.WriteLine();
+        }
+
+        public bool Shoot(int x, int y)
+        {
+            board[y, x] = true;
+            if (Ship.isShip[y, x])
+            {
+                int shipIndex = Ship.GetShipAtPoint(ships, x, y);
+                ships[shipIndex].destroyedParts++;
+                if (ships[shipIndex].destroyedParts == ships[shipIndex].size)
+                    DestroyShip(shipIndex);
+                return true;
+            }
+            return false;
+        }
+
+        public void DestroyShip(int shipIndex)
+        {
+            int horizontalMultiplier = ships[shipIndex].isHorizontal ? 1 : 0;
+            int verticalMultiplier = ships[shipIndex].isHorizontal ? 0 : 1;
+            for (int i = ships[shipIndex].initPoint[1] - 1; i <= ships[shipIndex].initPoint[1] + 1 * verticalMultiplier + ships[shipIndex].size * horizontalMultiplier; i++)
+            {
+                for (int j = ships[shipIndex].initPoint[0] - 1; j <= ships[shipIndex].initPoint[0] + 1 * horizontalMultiplier + ships[shipIndex].size * verticalMultiplier; j++)
+                    if (i >= 0 && j >= 0 && i < Board.size[0] && j < Board.size[1])
+                        board[i, j] = true;
+            }
+            destroyedShips++;
+            noMoreShipsFlag = destroyedShips == ships.Count;
         }
     }
 
     class Ship
     {
         static public bool[,] isShip = new bool[Board.size[0], Board.size[1]];
+        public int destroyedParts = 0;
         public int[] initPoint = new int[2];
         public bool isHorizontal;
         public int size;
@@ -82,9 +221,10 @@ namespace Battleships
         }
         private void CreateShip()
         {
-            int rand, horizontalMultiplier, verticalMultiplier;
+            int rand = 1, horizontalMultiplier = 1, verticalMultiplier = 0;
             bool isEmpty = false;
-            do
+            int tryLimiter = 0;
+            for (tryLimiter = 0; tryLimiter < 100 && !isEmpty; tryLimiter++)
             {
                 rand = Program.random.Next(2);
                 horizontalMultiplier = rand;
@@ -93,9 +233,9 @@ namespace Battleships
                 initPoint[0] = Program.random.Next(Board.size[1] - (size + 1) * verticalMultiplier);
                 initPoint[1] = Program.random.Next(Board.size[0] - (size + 1) * horizontalMultiplier);
                 isEmpty = true;
-                for (int i = initPoint[1] - 1; i <= initPoint[1] + 1 + size * horizontalMultiplier; i++)
+                for (int i = initPoint[1] - 1; i <= initPoint[1] + 1 * verticalMultiplier + size * horizontalMultiplier; i++)
                 {
-                    for (int j = initPoint[0] - 1; j <= initPoint[0] + 1 + size * verticalMultiplier; j++)
+                    for (int j = initPoint[0] - 1; j <= initPoint[0] + 1 * horizontalMultiplier + size * verticalMultiplier; j++)
                         if (i >= 0 && j >= 0 && i < isShip.GetLength(0) && j < isShip.GetLength(1))
                             if (isShip[i, j])
                             {
@@ -105,9 +245,39 @@ namespace Battleships
                     if (!isEmpty)
                         break;
                 }
-            } while (!isEmpty);
+            }
+            if (tryLimiter == 100)
+            {
+                Console.WriteLine("Too many ships...");
+                Console.ReadKey();
+                Environment.Exit(10);
+            }
             for (int i = 0; i < size; i++)
                 isShip[initPoint[1] + i * horizontalMultiplier, initPoint[0] + i * verticalMultiplier] = true;
+        }
+
+        public static int GetShipAtPoint(List<Ship> ships, int x, int y)
+        {
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].isHorizontal && ships[i].initPoint[0] == x)
+                {
+                    for (int j = 0; j < ships[i].size; j++)
+                    {
+                        if (ships[i].initPoint[1] + j == y)
+                            return i;
+                    }
+                }
+                if (!ships[i].isHorizontal && ships[i].initPoint[1] == y)
+                {
+                    for (int j = 0; j < ships[i].size; j++)
+                    {
+                        if (ships[i].initPoint[0] + j == x)
+                            return i;
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
