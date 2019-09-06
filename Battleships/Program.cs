15149f936076dfcd93afbf2006a8b0c0435e9b52
Yonatan Mankovich <yonimn2000@gmail.com>
2019-09-06 17:45:59 -0400

    Created Assembly Info.

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace Battleships
+namespace YonatanMankovich.Battleships
 {
     class Program
     {
